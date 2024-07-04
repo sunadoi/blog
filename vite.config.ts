5044b626563396f235a5c7d8e15cc8a4393eb4b2
sunadoi <y.sunadoi@gmail.com>
2024-07-04 21:12:44 +0900

    feat: ssg settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
-import { defineConfig } from "vite"
+import ssg from "@hono/vite-ssg"
 import honox from "honox/vite"
+import client from "honox/vite/client"
+import { defineConfig } from "vite"
 
-export default defineConfig({
-  plugins: [honox()],
+export default defineConfig(({ mode }) => {
+  return mode === "client"
+    ? {
+        plugins: [client()],
+      }
+    : {
+        build: { emptyOutDir: false },
+        plugins: [honox(), ssg({ entry: "./app/server.ts" })],
+      }
 })
