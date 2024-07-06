3283bcf94b495e624d115b0fd2d089c1c4f2cde2
sunadoi <y.sunadoi@gmail.com>
2024-07-06 11:43:55 +0900

    feat: radix colors

--- app/islands/counter.tsx
+++ app/islands/counter.tsx
@@ -1,13 +1,84 @@
-import { useState } from "hono/jsx"
+import { useState } from "react"
+import { Button } from "@/components/ui/button"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { Skeleton } from "@/components/ui/skeleton"
+import { Input } from "@/components/ui/input"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 export default function Counter() {
   const [count, setCount] = useState(0)
+
   return (
     <div>
       <p>Count: {count}</p>
-      <button type="button" onClick={() => setCount(count + 1)}>
+      <Button
+        onClick={() => {
+          setCount(count + 1)
+        }}
+      >
         Increment
-      </button>
+      </Button>
+      <Button
+        variant="secondary"
+        onClick={() => {
+          setCount(count + 1)
+        }}
+      >
+        Increment
+      </Button>
+      <Button variant="outline" disabled>
+        Disabled
+      </Button>
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
+      <div>
+        <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
+      </div>
+      <Tabs defaultValue="account" className="w-[400px]">
+        <TabsList>
+          <TabsTrigger value="account">Account</TabsTrigger>
+          <TabsTrigger value="password">Password</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">
+          Make changes to your account here.
+        </TabsContent>
+        <TabsContent value="password">Change your password here.</TabsContent>
+      </Tabs>
+      <Skeleton className="h-12 w-24 rounded-full" />
+      <Input placeholder="foo bar baz" />
     </div>
   )
 }
--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -8,7 +8,7 @@ export default reactRenderer(({ children, title }) => {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <Script src="/app/client.ts" async />
-        <link href="/app/style.css" rel="stylesheet" />
+        <link href="/app/styles/index.css" rel="stylesheet" />
         {title && <title>{title}</title>}
       </head>
       <body>{children}</body>
