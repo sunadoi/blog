cf8c0adbda7411db9189ebc734fe9a896652ff97
sunadoi <y.sunadoi@gmail.com>
2024-07-13 14:00:21 +0900

    feat: sp menu

--- app/components/projects/Header.tsx
+++ app/components/projects/Header.tsx
@@ -1,32 +1,38 @@
 import { tv } from "tailwind-variants"
 import { ThemeButton } from "../parts/_Theme.island"
+import { Menu } from "./_Menu.island"
 
 export const Header = () => {
-  const { base, title, links } = header({
+  const { base, title, links, menuIcon } = header({
     linksDisplay: { initial: "hidden", sm: "visible" },
   })
 
   return (
-    <header className={base()}>
-      <a href="/">
-        <h1 className={title()}>Suna's Box</h1>
-      </a>
-      <div class="flex items-center gap-8">
-        <ul className={links()}>
-          <li>
-            <a href="/about">
-              <h2>About</h2>
-            </a>
-          </li>
-          <li>
-            <a href="/tags">
-              <h2>Tags</h2>
-            </a>
-          </li>
-        </ul>
-        <ThemeButton />
+    <>
+      <header className={base()}>
+        <a href="/">
+          <h1 className={title()}>Suna's Box</h1>
+        </a>
+        <div class="flex items-center gap-8">
+          <ul className={links()}>
+            <li>
+              <a href="/about">
+                <h2>About</h2>
+              </a>
+            </li>
+            <li>
+              <a href="/tags">
+                <h2>Tags</h2>
+              </a>
+            </li>
+          </ul>
+          <ThemeButton />
+        </div>
+      </header>
+      <div class={menuIcon()}>
+        <Menu />
       </div>
-    </header>
+    </>
   )
 }
 
@@ -36,6 +42,7 @@ const header = tv(
       base: "flex border-b-2 items-center justify-between w-full bg-background py-4",
       title: "font-semibold",
       links: "",
+      menuIcon: "",
     },
     variants: {
       linksDisplay: {
@@ -43,11 +50,13 @@ const header = tv(
           base: "h-16",
           title: "text-lg",
           links: "hidden",
+          menuIcon: "block",
         },
         visible: {
           base: "h-20",
           title: "text-3xl",
           links: "flex gap-8 items-center",
+          menuIcon: "hidden",
         },
       },
     },
---
+++ app/components/projects/_Menu.island.tsx
@@ -0,0 +1,107 @@
+import clsx from "clsx"
+import { useState } from "hono/jsx"
+
+export const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <>
+      <button
+        type="button"
+        class={clsx(
+          "fixed z-10 border rounded-full bg-popover left-3 p-5",
+          "bottom-[calc(env(safe-area-inset-bottom,0px)+24px)]",
+        )}
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        <div class="flex flex-col justify-around w-8 h-8">
+          <span class="w-full h-0.5 bg-foreground" />
+          <span class="w-full h-0.5 bg-foreground" />
+          <span class="w-full h-0.5 bg-foreground" />
+        </div>
+      </button>
+      {isOpen && (
+        <>
+          {/* biome-ignore lint/a11y/useKeyWithClickEvents: */}
+          <div
+            class="fixed z-10 bg-transparent w-full h-full"
+            onClick={() => setIsOpen(false)}
+          />
+          <div class="fixed z-10 rounded-lg bg-popover bottom-[calc(env(safe-area-inset-bottom,0px)+96px)] left-3 p-2">
+            <ul class="flex flex-col gap-4 p-4">
+              <li>
+                <a href="/" class="flex items-center gap-2">
+                  <HomeIcon />
+                  <p>Home</p>
+                </a>
+              </li>
+              <li>
+                <a href="/about" class="flex items-center gap-2">
+                  <AccountIcon />
+                  <p>About</p>
+                </a>
+              </li>
+              <li>
+                <a href="/tags" class="flex items-center gap-2">
+                  <TagsIcon />
+                  <p>Tags</p>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </>
+      )}
+    </>
+  )
+}
+
+export const HomeIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+    >
+      <title>Home Icon</title>
+      <path
+        fill="currentColor"
+        d="m12 5.69l5 4.5V18h-2v-6H9v6H7v-7.81zM12 3L2 12h3v8h6v-6h2v6h6v-8h3"
+      />
+    </svg>
+  )
+}
+
+export const AccountIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+    >
+      <title>Account Icon</title>
+      <path
+        fill="currentColor"
+        d="M12 4a4 4 0 0 1 4 4a4 4 0 0 1-4 4a4 4 0 0 1-4-4a4 4 0 0 1 4-4m0 2a2 2 0 0 0-2 2a2 2 0 0 0 2 2a2 2 0 0 0 2-2a2 2 0 0 0-2-2m0 7c2.67 0 8 1.33 8 4v3H4v-3c0-2.67 5.33-4 8-4m0 1.9c-2.97 0-6.1 1.46-6.1 2.1v1.1h12.2V17c0-.64-3.13-2.1-6.1-2.1"
+      />
+    </svg>
+  )
+}
+
+export const TagsIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+    >
+      <title>Tag Icon</title>
+      <path
+        fill="currentColor"
+        d="M6.5 10C7.3 10 8 9.3 8 8.5S7.3 7 6.5 7S5 7.7 5 8.5S5.7 10 6.5 10M9 6l7 7l-5 5l-7-7V6zm0-2H4c-1.1 0-2 .9-2 2v5c0 .6.2 1.1.6 1.4l7 7c.3.4.8.6 1.4.6s1.1-.2 1.4-.6l5-5c.4-.4.6-.9.6-1.4c0-.6-.2-1.1-.6-1.4l-7-7C10.1 4.2 9.6 4 9 4m4.5 1.7l1-1l6.9 6.9c.4.4.6.9.6 1.4s-.2 1.1-.6 1.4L16 19.8l-1-1l5.7-5.8z"
+      />
+    </svg>
+  )
+}
