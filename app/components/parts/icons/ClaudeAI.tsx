e960a698d921cd45ff0283ec7dedaae17b8a7dd1
sunadoi <y.sunadoi@gmail.com>
2025-07-27 14:53:24 +0900

    feat: claude icon

---
+++ app/components/parts/icons/ClaudeAI.tsx
@@ -0,0 +1,10 @@
+export const ClaudeAIIcon = () => {
+  return (
+    <img
+      src="/assets/claude-ai-icon.svg"
+      alt="Claude AI Icon"
+      width="100%"
+      height="100%"
+    />
+  )
+}
